2b23bf4037cf45dce74f79bfd31ff5c5e1dc916a
issarsaggu8116-sketch <issarsaggu8116@gmail.com>
2026-05-22 17:46:08 +0530

    Delete routes directory

--- routes/deliveryAuthRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginDeliveryPartner } from "../controllers/deliveryAuthController.js";
-
-const router = express.Router();
-
-router.post("/login", loginDeliveryPartner);
-
-export default router;
